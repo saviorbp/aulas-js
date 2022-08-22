368df3cc9e46bcc62d85b52cc5b23807c9e2c1e8
saviorbp <72775597+saviorbp@users.noreply.github.com>
2022-08-22 18:50:14 -0300

    comcluçao da aula 1 e 2

---
+++ aula 2 - variaveis/script.js
@@ -0,0 +1,69 @@
+// tipos primitivos
+
+//boolean valor
+var vouf = false;
+console.log(vouf);
+
+//boolean tipo
+var vouf = false;
+console.log(typeof(vouf));
+
+//number valor
+var numeroQ = 10;
+console.log(numeroQ);
+
+//number tipo
+var numeroQ = 10;
+console.log(typeof(numeroQ));
+
+//string valor
+var texto = "texto";
+console.log(texto);
+
+//string tipo
+var texto = "texto";
+console.log(typeof(texto));
+
+// function valor
+var funcao = function(){};
+console.log(funcao);
+
+// function tipo
+var funcao = function(){};
+console.log(typeof(funcao));
+
+//como declarar uma variavel var
+var variavel = 'diana';
+console.log(variavel);
+variavel = 'maria';
+console.log(variavel);
+
+//como declarar uma variavel let
+let variavellet = 'diana';
+console.log(variavellet);
+
+//como declarar uma variavel const
+const variavelconst = 'diana';
+console.log(variavelconst);
+
+var escopoglobal = 'global';
+console.log(escopoglobal);
+
+function escopolocal(){
+  let escopolocalinteiro = 'local';
+  console.log(escopolocalinteiro);
+}
+
+escopolocal();
+
+//atribuiçao
+var atribuiçao= 'diana';
+
+// comparaçao
+var comparaçao = '0'==0;
+console.log(comparaçao);
+
+//comparaçao identica
+var comparaçaoidentica = '0'===0;
+console.log(comparaçaoidentica);
+
